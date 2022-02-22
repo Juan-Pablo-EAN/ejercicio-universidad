5228ae2bd4b6813105d3e6055fbd14b4ae97a6b4
Juan Pablo Ballesteros <j.ballesteros@dxc.com>
2022-02-22 14:43:22 -0500

    inicio

---
+++ app.js
@@ -0,0 +1,9 @@
+/**
+ * Sistema de registro de notas
+ * 
+ * Funciones:
+ * Registrar la nota de 5 estudiantes en 5 asignaturas diferentes
+ * Obtener la nota definitiva de cada asignatura. 3 de esas 5 notas tienen el mismo valor 
+ * Promedio general de cada estudiante en todas sus asignaturas
+ * Promedio de la nota definitiva de todos los estudiantes por cada asignatura
+ */
